3448cb9793625be79efbabc21f73e07a270e7a2a
Md. Soliman Ali <soliman15-8572@diu.edu.bd>
2022-08-15 13:29:09 +0600

    Add files via upload

--- Contact_Management/src/app/app-routing.module.ts
+++ Contact_Management/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactManagerComponent } from './contact-manager/contact-manager.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/blood/admin', pathMatch: 'full' },
+  { path: 'blood/admin', component: ContactManagerComponent },
+  { path: 'blood/add', component: AddContactComponent },
+  { path: 'user/edit/:contactID', component: EditContactComponent },
+  { path: 'user/view/:contactID', component: ViewContactComponent },
+  { path: '**', component: ErrorPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { }
---
+++ Contact_Management/src/app/models/myContact.ts
@@ -0,0 +1,13 @@
+export class MyContact {
+    id?: string;
+    photo!: string;
+    name!: string;
+    designation!: string;
+    gender!: string;
+    age!: string;
+    mobile_no!: string;
+    blood_group!: string;
+    email!: string;
+    required_date!: string;
+    address!: string;
+}
