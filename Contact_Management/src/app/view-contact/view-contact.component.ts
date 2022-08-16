539a519668305e0475d7af14cfb698e85820290a
Md-Soliman-Ali <soliman15-8572@diu.edu.bd>
2022-08-16 15:57:16 +0600

    Add files via upload

--- Contact_Management/src/app/view-contact/view-contact.component.ts
+++ Contact_Management/src/app/view-contact/view-contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MyContact } from '../models/myContact';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-view-contact',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewContactComponent implements OnInit {
 
-  constructor() { }
+  public contactID: string | null = null;
+  public loading: boolean = false;
+  public contact: MyContact = {} as MyContact;
+  public errorMessage: null;
+
+  constructor(private activatedRoute: ActivatedRoute, private contService: ContactService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((param) => {
+      this.contactID = param.get('contactID')
+    });
+    if (this.contactID) {
+      this.loading = true;
+      this.contService.getUser(this.contactID).subscribe((data: MyContact) => {
+        this.contact = data;
+        this.loading = false;
+      }, (error) => {
+        this.errorMessage = error;
+        this.loading = false;
+      })
+    }
   }
-
+  public isNotEmpty() {
+    return Object.keys(this.contact).length > 0;
+  }
 }
